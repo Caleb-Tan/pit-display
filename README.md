44a189e814b9da14cef16d8311815a09465c1d1e
Caleb-Tan <calebtan23@gmail.com>
2018-01-13 20:31:15 -0800

    added project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# pit-display
+# Pit Display
+
+# How to run:
+To grab the match schedule and data, run `python grab_matches.py`, then input the blue alliance match key.
+
+To run the website, start a simple http python server. `python -m SimpleHTTPServer [PORT]` Chrome only supports http, data, and https cross origin requests to read from the text file. 
+
+
---
+++ grab_matches.py
@@ -0,0 +1,43 @@
+import requests, json, ast
+import pprint
+import time
+
+pp = pprint.PrettyPrinter(indent=1)
+
+def write_to_file():
+    event_key = str(input("Enter event key:"))
+    matches=convert_unicode(requests.get("http://www.thebluealliance.com/api/v3/event/"+event_key+"/matches/simple", headers={"X-TBA-Auth-Key":"v62ZvFvAkyWVH1qwd1m8Kyyhll0VEvyxUGo7pqpM1re827yjVZlgtjdpBEQJNcn2"}).json())
+    match_list_qm = {}
+    for match in matches:
+        if match.get('comp_level') == 'qm':
+            match_qm = []
+            ts = time.strftime('%I:%M%p', time.localtime(match.get('time'))).lstrip('0').lower()
+            blue_teams = match.get('alliances').get('blue').get('team_keys')
+            red_teams = match.get('alliances').get('red').get('team_keys')
+            match_number = match.get('match_number')
+            match_id = match_number
+
+            match_qm.extend((ts, blue_teams, red_teams))
+            match_list_qm[match_id] = match_qm
+        
+    match_file = open(r"matches-svr.txt","a")
+    for key in sorted(match_list_qm.keys()):
+        match = "%s %s" % (key, match_list_qm[key])
+        match = match.translate(None, '[],\'frc') # get rid of certain characters
+        match_file.write(match)
+        match_file.write("\n")
+    
+    match_file.close()
+
+
+def convert_unicode(input):
+    if isinstance(input, dict):
+        return {convert_unicode(key): convert_unicode(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert_unicode(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
+
+write_to_file()
